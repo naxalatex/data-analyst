113e3819c779ba171e1e403cbdec3242275db3db
sidharthsivacochin@gmail.com <sidharthsivacochin@gmail.com>
2025-09-17 11:19:19 +0530

    minor updates

---
+++ datascience.py
@@ -0,0 +1,23 @@
+class calculation:
+    def add(self,a,b):
+        return a+b
+    
+    def subtract(self,a,b):
+        return a-b
+    
+    def product(self,a,b):
+        return a*b
+    
+    def divide (self,a,b):
+        return a/b
+
+class word:
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+    def show(self):
+        print(self.add())
+        print(self.subtract())
+        print(self.product())
+        print(self.divdie())
+animals = ['fox','cat','dog']
